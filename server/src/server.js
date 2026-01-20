feb4500b90478a56ca06769a0ee512ca0482237e
alexander-p1 <alexanderpettersson_@hotmail.com>
2026-01-20 15:38:03 +0100

    Express och db boilerplate

---
+++ server/src/server.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
